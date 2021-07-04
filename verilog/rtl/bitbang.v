830b005fa88dadca9766c3a4010851278e2710ad
Your Name <andrewattwood@gmail.com>
2021-07-04 01:51:59 +0100

    moved the gds to correct location

--- verilog/rtl/bitbang.v
+++ verilog/rtl/bitbang.v
@@ -1,3 +1,18 @@
+// SPDX-FileCopyright 2021 Nguyen Dao
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
 module bitbang (s_clk, s_data, strobe, data, active, clk);
 	localparam on_pattern = 16'hFAB1; 
 	localparam off_pattern = 16'hFAB0; 
